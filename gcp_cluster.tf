73e46f99da831e0f80435ac0bcf34c363fb3e7a8
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-02-12 18:35:41 -0500

    Update gcp_cluster.tf

--- gcp_cluster.tf
+++ gcp_cluster.tf
@@ -1,8 +1,4 @@
-# Configure the Google provider
-provider "google" {
-  project = "speedy-toolbox-360600"
-  region  = "us-central1"
-}
+
 
 # Create 4 GKE cluster instances
 resource "google_container_cluster" "primary" {
